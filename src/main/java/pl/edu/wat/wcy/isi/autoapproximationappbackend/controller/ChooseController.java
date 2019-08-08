364bd8014d0a74af0c9873a7ac0a4b8b89ad6494
Dawid Sapierzyński <44416017+dawidsapierzynski@users.noreply.github.com>
2019-08-08 21:01:00 +0200

    init

---
+++ src/main/java/pl/edu/wat/wcy/isi/autoapproximationappbackend/controller/ChooseController.java
@@ -0,0 +1,8 @@
+package pl.edu.wat.wcy.isi.autoapproximationappbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChooseController {
+
+}
