4d0faeebb5fccf1bb96ccf7621a98b189898b88d
Dawid Sapierzyński <dawid.sapierzynski@student.wat.edu.pl>
2019-08-08 21:11:42 +0200

    Fixes #1

---
+++ src/main/java/pl/edu/wat/wcy/isi/autoapproximationappbackend/AutoApproximationAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.wcy.isi.autoapproximationappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoApproximationAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoApproximationAppBackendApplication.class, args);
+    }
+
+}
